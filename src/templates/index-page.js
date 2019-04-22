59788d5e4c1261d7b05c63288b6396bedef7643b
Christopher Traver <ctraver@wayfair.com>
2019-04-22 16:30:05 -0400

    Wrapping up styling

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -63,21 +63,7 @@ export const IndexPageTemplate = ({ image, title, heading, subheading, mainpitch
                     <h1 className="title">{mainpitch.title}</h1>
                   </div>
                   <div className="tile">
-                    <h3 className="subtitle">{mainpitch.description}</h3>
-                  </div>
-                </div>
-                <div className="columns">
-                  <div className="column is-12">
-                    <h3 className="has-text-weight-semibold is-size-2">{heading}</h3>
-                    <p>{description}</p>
-                  </div>
-                </div>
-                <Features gridItems={intro.blurbs} />
-                <div className="columns">
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/products">
-                      See all products
-                    </Link>
+                    <p className="subtitle">{mainpitch.description}</p>
                   </div>
                 </div>
                 <div className="column is-12">
